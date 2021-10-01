2f73cdeb1d20baff879c2b96aec0bdfcc4b56e30
stevenneuhaus <stevenneuhaus@vagelos-ve703-06196.apn.wlan.private.upenn.edu>
2021-10-01 17:23:25 -0400

    adds working motor test (with new motor driver board)

--- test/motor_test/motor_test.ino
+++ test/motor_test/motor_test.ino
@@ -1,21 +1,30 @@
+
+const int OUT1 = 8;
+const int OUT2 = 9;
+const int ENA = 10; //PWM, ENA pin
 void setup() {
 
   //Set up for motors
-  pinMode(8,OUTPUT); // when high, reverse
-  pinMode(9,OUTPUT); //when high, forward
-  pinMode(10, OUTPUT); //PWM
+  pinMode(OUT1,OUTPUT); // when high, reverse
+  pinMode(OUT2,OUTPUT); //when high, forward
+  pinMode(ENA, OUTPUT); //PWM controls speed of motor
 }
 
 void loop() {
   // test if motor runs. first backwards then forwards.
-  digitalWrite(8,HIGH);
-  digitalWrite(9,LOW);
-  //analogWrite(10,255);
+  digitalWrite(OUT1,HIGH);
+  digitalWrite(OUT2,LOW);
+  analogWrite(ENA,255);
   delay(2000);
 
-  digitalWrite(8,LOW);
-  digitalWrite(9,HIGH);
-  //analogWrite(10,255);
+  digitalWrite(OUT1,LOW);
+  digitalWrite(OUT2,LOW);
+  analogWrite(ENA,0);
+  delay(1000);
+
+  digitalWrite(OUT1,LOW);
+  digitalWrite(OUT2,HIGH);
+  analogWrite(ENA,255);
   delay(2000);
 
 }
